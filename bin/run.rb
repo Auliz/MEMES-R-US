5d0a43cf6509b0f6b7d94c886695c952631c0c76
Auliz <joseph.auz77@gmail.com>
2020-04-14 14:07:28 -0500

    added comments and cleaned up run.rb

--- bin/run.rb
+++ bin/run.rb
@@ -1,26 +1,40 @@
 require_relative '../config/environment'
 
 puts "Please enter your first and last name:"
+# takes in user input in the form of a string ex. "Stefani Waddell"
 user_name_input = gets.chomp
+# searches the users table for the name that was inputted, and if no such row exists, it creates one
 user = User.find_or_create_by(name: "#{user_name_input}")
 
 
 puts "How are you feeling today?\n"
 puts "Mood list:"
+
+# pulls all of the names from the moods table and puts them into an array
 mood_list = Mood.pluck :name
+# takes the above array (mood_list) and makes a nice numbered list as output
 mood_list.each.with_index(1) do |value, index|
   puts "#{index}: #{value}"
 end
 
 puts "\nPlease enter a moods' number from the above list: "
 
-# user input - 1 to search the array of moods 
+# takes in user input in the form of a string ex. "3"
+user_mood_input = gets.chomp
+# subtracts 1 from the user input to match the index pattern of an array (arrays start at 0 and our list starts at 1)
+mood_search = user_mood_input.to_i - 1
+# uses the above index to search the array and get the name of the mood
+mood_input_name = mood_list[mood_search]
 
-# take mood name and search the databse for it's id
-user_mood_id_from_input = Mood.all.find_by(name: "meh").id 
+# gets the primary key from the mood table for the mood that the user entered
+user_mood_id_from_input = Mood.all.find_by(name: "#{mood_input_name}").id 
 
-user_mood_input = gets.strip
+# gets the primary key of the current user
+user_id_from_input = User.all.find_by(name: "#{user_name_input}").id
 
-user.mood_id = mood.id
+# creates (aka .new and .save) the join of the mood, user, and meme
+FinalKey.create(mood_id: "#{user_mood_id_from_input}", user_id: "#{user_id_from_input}")
 
-# Final_key.create(mood: user_mood_input, name: user_name_input)
+binding.pry
+
+
