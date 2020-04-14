6eec7e812bdb44757aafa53af43bcc943469bcaf
Auliz <joseph.auz77@gmail.com>
2020-04-14 15:17:38 -0500

    made memeage return things

--- bin/memes_api.rb
+++ bin/memes_api.rb
@@ -2,11 +2,11 @@ require 'net/http'
 require 'open-uri'
 require 'json'
 require 'pry'
-
+require_relative 'app/models/meme'
  
 class GetMemes
- 
-  URL = "http://alpha-meme-maker.herokuapp.com/"
+  page_number = rand(1..10)
+  URL = "http://alpha-meme-maker.herokuapp.com/#{page_number}/"
  
   def get_memes
     uri = URI.parse(URL)
@@ -14,17 +14,12 @@ class GetMemes
     response.body
   end
 
-
-def program_memes
-  # we use the JSON library to parse the API response into nicely formatted JSON
+  def program_memes
+    meme_number = rand(1..23)
     programs = JSON.parse(self.get_memes)
-    #programs.each do |program, memes, idk|
-      binding.pry
-      programs["data"][0]["image"]
+    programs["data"][meme_number]["image"]
+  end
 end
-end
+# programs = GetMemes.new
 
-#programs = GetMemes.new.get_memes
-#puts programs
-programs = GetMemes.new
-puts programs.program_memes.uniq
+# binding.pry
