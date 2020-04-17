67f269b4513c0cc2ce9006553a84b7de883541d7
hr0dz <haley.rodzos@gmail.com>
2020-04-17 12:58:26 -0500

    still a piece of garbageeeeee. also new boat

--- bin/tty_test.rb
+++ bin/tty_test.rb
@@ -1,8 +1,10 @@
 require "tty-prompt"
 require 'pry'
 
-prompt = TTY::Prompt.new
+#prompt = TTY::Prompt.new
 
-shit = prompt.select("Choose your destiny?", %w(Scorpion Kano Jax))
+#hit = prompt.select("Choose your destiny?", %w(Scorpion Kano Jax))
 
-binding.pry
+#binding.pry
+
+pid = fork{ exec 'afplay', "/Users/haley/Downloads/The Fresh Prince Of Bel Air Theme Song Full-[AudioTrimmer.com]-[AudioTrimmer.com].mp3" }
